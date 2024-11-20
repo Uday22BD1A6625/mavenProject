ae4ac825d4ee4af3863f05c45d6b1a874411844d
Uday22BD1A6625 <githubselab@gmail.com>
2024-11-20 13:46:11 +0530

    First commit to maven java project

---
+++ src/main/java/com/mavenweb/SEProject/App.java
@@ -0,0 +1,13 @@
+package com.mavenweb.SEProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "This is the first Maven Java Project in Eclipse" );
+    }
+}
